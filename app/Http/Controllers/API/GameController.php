eb614a59ffe7defe7b15539dc691343ac722284c
IgnReLoaD <ortiz.ignasi@gmail.com>
2023-02-22 01:42:38 +0100

    features gamecontroller play

--- app/Http/Controllers/API/GameController.php
+++ app/Http/Controllers/API/GameController.php
@@ -1,27 +1,26 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+// use Laravel\Passport\HasApiTokens;
 use App\Models\User;
 use App\Models\Game;
 
 class GameController extends Controller
 {
-    /**
-     * Postman:  POST /players/{id}/games  
-     * @param  int  $id_player
-     * @return \Illuminate\Http\Response --> message
-     */
+    // Exercice-5 ... POST /players/{id}/games --> un jugador/a específic realitza una tirada dels daus.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok (i graba en MySql tabla Games)
     public function play($id_player)
     {
-        return response([
-            'message' => 'entra en GameController::play'
-        ]);
-        die;
-
+        // return response([
+        //     'message' => 'entra en GameController::play'
+        // ]);
+        // die;
 
         $idUserLoggedIn = Auth::user()->id;
 
@@ -43,97 +42,212 @@ class GameController extends Controller
 
         }else{
             return response([
-                'message' => 'user not found' 
+                'advise' => 'User ' . $id_player . ' not found, or at least not logged in.',
+                'clue' => 'The currently logged user is the number ' . $idUserLoggedIn
             ]);
         }
     }
 
-
-     /**
-     * Entrar en Postman en BODY .
-     * @param  int  $id_user
-     * @return \Illuminate\Http\Response  --> message
-     */
-
-    public function destroyUserScore($id_user)
+    // Exercice-6 ... GET /players/{id}/games --> retorna el llistat de jugades per un jugador/a.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok (si OK retorna average d'encert i les tirades).
+    public function showPlayerScore($id_player)
     {
-        return response([
-            'message' => 'entra en GameController::destroyUserScore'
-        ]);
-        die;
+        $idUserLoggedIn = Auth::id();
+        if ($idUserLoggedIn != $id_player) { 
+            return response([
+                'advise' => 'User ' . $id_player . ' not found, or at least not logged in.',
+                'clue' => 'User ' .$idUserLoggedIn. ' is the real who is logged in right now.'
+            ]);
+        }else{
+            $objCurrentUsr = User::find($id_player);
+            $gamesByPlayer = Game::where('user_id', $id_player)->get();
+            if ($gamesByPlayer->isEmpty()) {
+                return response([
+                    'message' => 'This player has not games yet.'
+                ]);            
+            }
+            $countAllGames = $gamesByPlayer->count();
+            $countWonGames = $gamesByPlayer->where('boolWinner', true)->count();
+            $successAverage = ($countWonGames / $gamesByPlayer->count()) * 100;
+            return response([
+                'current user logged' => $objCurrentUsr->name,
+                'game attempts' => $countAllGames,
+                'success average' => $successAverage . '%',
+                'game list' => $gamesByPlayer
+            ]);
+        }
+    }
+
+    // Exercice-7 ... DELETE /players/{id}/games --> elimina les tirades del jugador/a loguejat.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok (si OK graba en MySql tabla Games - borra)
+    public function delPlayerScore($id_player)
+    {
+        // return response([
+        //     'message' => 'entra en GameController::delPlayerScore'
+        // ]);
+        // die;
 
         $idUserLoggedIn = Auth::id();
-        $objUser = User::find($id_user);
+        $objUser = User::find($id_player);
 
         if (!$objUser) {
             return response([
                 'message' => 'User not found, could not delete their scores!',
                 'status' => 404,
             ]);
-        } elseif($idUserLoggedIn == $id_user) {
-            $userGames = Game::where('user_id', '=', $id_user)->first('id');
-
+        } elseif($idUserLoggedIn == $id_player) {
+            $userGames = Game::where('user_id', '=', $id_player)->first('id');
             if($userGames !== null) {
-                Game::where('user_id', $id_user)->delete();
-                return response(['message' => 'Games deleted from user ' . $objUser->name]);            
+                Game::where('user_id', $id_player)->delete();
+                return response(['message' => 'Games of player number ' . $objUser->id . ", called " . $objUser->name . ', sucessfully deleted.']);    
             } else {
-                return response(['message' => "The user " . $objUser->name . " don't have any game!"]);
+                return response(['message' => "The player number " . $objUser->id . ", called " . $objUser->name . ", don't have any game!"]);
             }
         }
     }
 
+    // Exercice-8 ... GET /players --> retorna el llistat de tots els jugadors del sistema amb el percentatge mitjà d’èxit.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok (avisant si l'usuari loguejat és Admin o no)
     public function allPlayersScore()
     {
-        $users = User::all();
-        $arrUsers = [];
 
-        foreach ($arrUsers as $user) {
-            // total partides jugades i total de guanyades
-            $userGames = Game::where('user_id',$user->id)->get();
-            $countGames = $userGames->count();
-            $countWonGames = $userGames->where('boolWinner',true)->count();
+        $idUserLoggedIn = Auth::id();
+        $objUser = User::find($idUserLoggedIn);
 
-            // percentatge d'encert
-            $successAverage = 0;
-            if ($countGames > 0) {
-                $successAverage = ($countWonGames / $countGames) * 100;
+        if (!$objUser->sysadmin){
+            return response()->json([
+                'message' => "Current logged user is " . $idUserLoggedIn . ", called " . $objUser->name . ", is not SysAdmin. So he/she cannot see the info related to other players."
+            ]);
+        }else{
+            $allUsers = User::all();
+            $arrUsers = [];
+
+            foreach ($allUsers as $user) {
+                // total partides jugades i total de guanyades
+                $userGames = Game::where('user_id',$user->id)->get();
+                $countGames = $userGames->count();
+                $countWonGames = $userGames->where('boolWinner',true)->count();
+
+                // percentatge d'encert
+                $successAverage = 0;
+                if ($countGames > 0) {
+                    $successAverage = ($countWonGames / $countGames) * 100;
+                }
+
+                // preparar dades d'un jugador
+                $userDetails = [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'sysadmin' => $user->sysadmin ? true : false,
+                    'attempts' => $countGames,
+                    'success' => $countWonGames,
+                    'average' => $successAverage.'%'
+                ];
+
+                // afegir al llistat
+                array_push($arrUsers, $userDetails);
             }
 
-            // preparar dades
-            $userDetails = [
-                'id' => $user->id,
-                'name' => $user->name,
-                'email' => $user->email,
-                'average' => $successAverage
-            ];
-            array_push($arrUsers, $userDetails);
+            // sortida de dades
+            return response()->json([
+                'message' => "Current logged user is " . $idUserLoggedIn . ", called " . $objUser->name . ", and is SysAdmin. So he/she can see the following info related to other players.",
+                'players' => $arrUsers,
+                'status'  => 200
+            ]);
         }
-
-        // sortida de dades
-        return response()->json([
-            'players' => $arrUsers,
-            'status'  => 200
-        ]);
     }
 
-    public function showPlayerScore($id_player)
-    {
-        $authUser = Auth::id();
-        $gamesByPlayer = Game::where('user_id', $id_player)->get();
+    // Exercice-9 ... GET /players/ranking: retorna el rànquing mitjà de tots els jugadors del sistema. És a dir, el percentatge mitjà d’èxits.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok amb el Llistat Ranking (avisa si l'usuari loguejat és Admin o no).
+    public function allPlayersRanking(){
+        // return response([
+        //     'message' => 'entra en GameController::allPlayersRanking'
+        // ]);
+        // die;
 
-        if ($gamesByPlayer->isEmpty()) {
-            return response([
-                'message' => 'This player has not games yet'
+        $idUserLoggedIn = Auth::id();
+        $objUser = User::find($idUserLoggedIn);
+
+        if (!$objUser->sysadmin){
+            return response()->json([
+                'message' => "Current logged user is " . $idUserLoggedIn . ", called " . $objUser->name . ", is not SysAdmin. So he/she cannot see the info related to other players."
+            ]);
+        }else{
+            $fieldsetAttempts = DB::select("SELECT COUNT(*) as fieldAttempts FROM games");
+            $fieldsetSuccess = DB::select("SELECT COUNT(*) as fieldSuccess FROM games WHERE boolwinner='1'");
+            $average = $fieldsetSuccess[0]->fieldSuccess / $fieldsetAttempts[0]->fieldAttempts * 100;
+            return response()->json([
+                'message' => "Current logged user is " . $idUserLoggedIn . ", called " . $objUser->name . ", and is SysAdmin. So can see the following info related to other players.",
+                'attempts all players' => $fieldsetAttempts[0]->fieldAttempts,
+                'success all players' => $fieldsetSuccess[0]->fieldSuccess,
+                'Avg ranking all players' => round($average,4) . ' %',
+                'status'  => 200
+            ]);
+        }
+    }
+
+    // Exercice-10... GET /players/ranking/loser: retorna el jugador/a amb pitjor percentatge d’èxit.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok amb el Loser (avisa si l'usuari loguejat és Admin o no).
+    public function loserPlayer(){
+        $idUserLoggedIn = Auth::id();
+        $objUser = User::find($idUserLoggedIn);
+
+        if (!$objUser->sysadmin){
+            return response()->json([
+                'message' => "Current logged user is " . $idUserLoggedIn . ", called " . $objUser->name . ", is not SysAdmin. So he/she cannot see the info related to other players."
+            ]);
+        }else{
+            $resultLoserPlayer = DB::select("
+                SELECT user_id, COUNT(*) as attempts_success FROM games
+                WHERE boolWinner=0
+                GROUP BY user_id 
+                ORDER BY attempts_success DESC
+                LIMIT 1
+            ");
+            $objLoserPlayer = User::find($resultLoserPlayer[0]->user_id);
+
+            return response()->json([
+                'message' => "Current logged user is " . $idUserLoggedIn . ", called " . $objUser->name . ", and is SysAdmin. So can see the following info related to other players.",                
+                'loser' => "The bad player is number " . $objLoserPlayer->id . ", called " . $objLoserPlayer->name . "!!",
+                'status'  => 200
+            ]);
+        }
+    }
+
+    // Exercice-11... GET /players/ranking/winner: retorna el jugador/a amb millor percentatge d’èxit.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok amb el Winner (avisa si l'usuari loguejat és Admin o no). 
+    public function winnerPlayer(){
+        $idUserLoggedIn = Auth::id();
+        $objUser = User::find($idUserLoggedIn);
+
+        if (!$objUser->sysadmin){
+            return response()->json([
+                'message' => "Current logged user is " . $idUserLoggedIn . ", called " . $objUser->name . ", is not SysAdmin. So he/she cannot see the info related to other players."
+            ]);
+        }else{
+            $resultWinnerPlayer = DB::select("
+                SELECT user_id, COUNT(*) as attempts_success FROM games
+                WHERE boolWinner=1
+                GROUP BY user_id
+                ORDER BY attempts_success DESC
+                LIMIT 1
+            ");
+            $objWinnerPlayer = User::find($resultWinnerPlayer[0]->user_id);
+
+            return response()->json([
+                'message' => "Current logged user is " . $idUserLoggedIn . ", called " . $objUser->name . ", and is SysAdmin. So can see the following info related to other players.",                
+                'winner' => "The best player is number " . $objWinnerPlayer->id . ", called " . $objWinnerPlayer->name . "!!",
+                'status'  => 200
             ]);            
         }
-
-        $countWonGames = $gamesByPlayer->where('boolWinner', true)->count();
-        $successAverage = ($countWonGames / $gamesByPlayer->count()) * 100;
-
-        return response([
-            'games' => $gamesByPlayer,
-            'success' => $successAverage . '%'
-        ]);
     }
+
 
 }
--- routes/api.php
+++ routes/api.php
@@ -5,27 +5,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\GameController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// ENUNCIAT: Has de tindre en compte els següents detalls de construcció, URL’s:
 
-
-// Has de tindre en compte els següents detalls de construcció:
-// URL’s:
-
-// POST /players : crea un jugador/a.
-// postman: nom, email, contrasenya, confirm-contrasenya
+// Exercice-1 ... POST /players --> crea un jugador/a. 
+//                @Params postman: nom, email, contrasenya, confirm-contrasenya
+//                @Retorna Message Ok/Nok (i graba en MySql tabla Users)
 Route::post('/players', [UserController::class, 'register'])->name('register');
-// postman: email, contrasenya
+
+// Exercice-2 ... POST /login --> login de jugador/a.
+//                @Params postman: email, contrasenya 
+//                @Retorna Bearer-Token (no graba res en MySql)
 Route::post('/login', [UserController::class, 'login'])->name('login');
 
+// --------------------
+// MIDDLEWARE PASSPORT:
+// --------------------
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -33,29 +27,53 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::middleware(['auth:api'])->group(function () {
 
-    // POST /logout --> li passem el bearer token
+    // Exercice-3 ... POST /logout --> desloguejar usuari jugador/a.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok
     Route::post('/logout', [UserController::class, 'logout'])->name('logout');
-    // PUT /players/{id} : modifica el nom del jugador/a.
+
+    // Exercice-4 ... PUT /players/{id} --> modifica el nom del jugador/a.
+    //                @Params postman: name i mail a modificar, i Bearer-Token per demostrar qui és.
+    //                @Retorna Message Ok/Nok (i graba en MySql tabla Users)
     Route::put('/players/{id}', [UserController::class, 'edit'])->name('edit');
 
-    // POST /players/{id}/games/ : un jugador/a específic realitza una tirada dels daus.
+    // Exercice-5 ... POST /players/{id}/games --> un jugador/a específic realitza una tirada dels daus.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok (i graba en MySql tabla Games)
     Route::post('/players/{id}/games',[GameController::class, 'play'])->name('play');
 
-    // DELETE /players/{id}/games: elimina les tirades del jugador/a.
-    Route::delete('/players/{id}/games',[GameController::class, 'destroyUserScore'])->name('destroyUserScore'); 
-
-    // GET /players: retorna el llistat de tots els jugadors/es del sistema amb el seu percentatge mitjà d’èxits 
-    Route::get('/players',[GameController::class, 'allPlayersScore'])->name('allPlayersScore');
-
-    // GET /players/{id}/games: retorna el llistat de jugades per un jugador/a.
+    // Exercice-6 ... GET /players/{id}/games --> retorna el llistat de jugades per un jugador/a.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok (si OK retorna average d'encert i les tirades)
     Route::get('/players/{id}/games', [GameController::class, 'showPlayerScore'])->name('showPlayerScore');
 
-    // GET /players/ranking: retorna el rànquing mitjà de tots els jugadors/es del sistema. És a dir, el percentatge mitjà d’èxits.
-    Route::get('/players/ranking', [GameController::class, 'ranking'])->name('ranking'); 
+    // Exercice-7 ... DELETE /players/{id}/games --> elimina les tirades del jugador/a loguejat.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok (si OK graba en MySql tabla Games - borra)
+    Route::delete('/players/{id}/games',[GameController::class, 'delPlayerScore'])->name('delPlayerScore'); 
 
-    // GET /players/ranking/loser: retorna el jugador/a amb pitjor percentatge d’èxit.
-    Route::get('/players/ranking/loser', [GameController::class, 'loser'])->name('loser');
+    // ----------------------
+    // ADMINISTRADOR DEL JOC:
+    // ----------------------
 
-    // GET /players/ranking/winner: retorna el jugador/a amb millor percentatge d’èxit.
-    Route::get('/players/ranking/winner', [GameController::class, 'winner'])->name('winner');
+    // Exercice-8 ... GET /players --> retorna el llistat de tots els jugadors del sistema amb el percentatge mitjà d’èxit.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok amb el Llistat (avisant si l'usuari loguejat és Admin o no).
+    Route::get('/players',[GameController::class, 'allPlayersScore'])->name('allPlayersScore');
+
+    // Exercice-9 ... GET /players/ranking: retorna el rànquing mitjà de tots els jugadors del sistema. És a dir, el percentatge mitjà d’èxits.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok amb el Llistat Ranking (avisa si l'usuari loguejat és Admin o no).
+    Route::get('/players/ranking', [GameController::class, 'allPlayersRanking'])->name('allPlayersRanking'); 
+
+    // Exercice-10... GET /players/ranking/loser: retorna el jugador/a amb pitjor percentatge d’èxit.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok amb el Loser (avisa si l'usuari loguejat és Admin o no).
+    Route::get('/players/loser', [GameController::class, 'loserPlayer'])->name('loserPlayer');
+
+    // Exercice-11... GET /players/ranking/winner: retorna el jugador/a amb millor percentatge d’èxit.
+    //                @Params postman: només el Bearer-Token que ens ha proporcionat el Login.
+    //                @Retorna Message Ok/Nok amb el Winner (avisa si l'usuari loguejat és Admin o no).    
+    Route::get('/players/winner', [GameController::class, 'winnerPlayer'])->name('winnerPlayer');
+
 });
