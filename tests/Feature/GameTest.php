d72968c89e1f2d3adfe1ae406878c4adbba673f3
IgnReLoaD <ortiz.ignasi@gmail.com>
2023-02-22 21:05:32 +0100

    separate the LearningTddTest in UserTest and GameTest

---
+++ tests/Feature/GameTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\Passport;
+use App\Models\User;
+use App\Models\Game;
+
+class GameTest extends TestCase
+{
+    // usamos un Trait (clase que agrupa funciones) se ejecuta cada vez después de ejecutar un Test, en este caso este sirve para Refresh
+    use RefreshDatabase;
+
+    // TDD Exercice-5 GAME-PLAY
+    public function logged_user_can_play_a_game()
+    {
+        // desactivar el manejador de excepciones porque sino el Test no me diría si/no, ya que lo captura el Catch del ExceptionHandling
+        $this->withoutExceptionHandling();
+        // simular la utilización del Passport
+        $this->artisan('passport:install');
+
+        // Create a single App\Models\User instance...
+        $objUser = User::factory()->create();
+        Passport::actingAs($objUser);
+
+        // mandar a un endPoint una llamada tipo Http POST a la URL 'players/{id_player}' pero NO le pasamos campos (sin Body del Postman)
+        $this->post('api/players/'. $objUser->id . '/games');
+
+        // para asertar que ha funcionado el Test, comprobar haya grabado en Tabla Games en Campo user_id con el valor ID del Logged-in User
+        $this->assertDatabaseHas('games',[
+            'user_id' => $objUser->id
+        ]);
+    }
+
+    // TDD Exercice-6 GAME-SHOW-PLAYER-SCORE
+    public function logged_user_can_show_his_games()
+    {
+        
+    }
+
+
+}
